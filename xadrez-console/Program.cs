2e59c2c906ac569196868101d5a61b2266231718
romariomirandadias <rmirandadias@hotmail.com>
2021-03-25 23:06:39 -0300

    Criando a primeira classe do projeto

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using tabuleiro;
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
         }
     }
 }
